16278e29777e0217269e4d757192fe9750c4cb32
Vaibhav-992501030195 <992501030195@mail.jiit.ac.in>
2026-07-01 15:43:04 +0530

    added build files

---
+++ build/CMakeFiles/ufc.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/audio_converter.cpp" "CMakeFiles/ufc.dir/src/audio_converter.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/audio_converter.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/converter.cpp" "CMakeFiles/ufc.dir/src/converter.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/converter.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/data_converter.cpp" "CMakeFiles/ufc.dir/src/data_converter.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/data_converter.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/detector.cpp" "CMakeFiles/ufc.dir/src/detector.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/detector.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/document_converter.cpp" "CMakeFiles/ufc.dir/src/document_converter.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/document_converter.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/history.cpp" "CMakeFiles/ufc.dir/src/history.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/history.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/image_converter.cpp" "CMakeFiles/ufc.dir/src/image_converter.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/image_converter.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/main.cpp" "CMakeFiles/ufc.dir/src/main.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/main.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/utils.cpp" "CMakeFiles/ufc.dir/src/utils.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/utils.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/validator.cpp" "CMakeFiles/ufc.dir/src/validator.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/validator.cpp.o.d"
+  "/Users/vaibhavpatidar/Desktop/oss/UniversalFileConverter/src/video_converter.cpp" "CMakeFiles/ufc.dir/src/video_converter.cpp.o" "gcc" "CMakeFiles/ufc.dir/src/video_converter.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ufc.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ufc.dir/src/audio_converter.cpp.o"
+  "CMakeFiles/ufc.dir/src/audio_converter.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/converter.cpp.o"
+  "CMakeFiles/ufc.dir/src/converter.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/data_converter.cpp.o"
+  "CMakeFiles/ufc.dir/src/data_converter.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/detector.cpp.o"
+  "CMakeFiles/ufc.dir/src/detector.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/document_converter.cpp.o"
+  "CMakeFiles/ufc.dir/src/document_converter.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/history.cpp.o"
+  "CMakeFiles/ufc.dir/src/history.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/image_converter.cpp.o"
+  "CMakeFiles/ufc.dir/src/image_converter.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/main.cpp.o"
+  "CMakeFiles/ufc.dir/src/main.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/utils.cpp.o"
+  "CMakeFiles/ufc.dir/src/utils.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/validator.cpp.o"
+  "CMakeFiles/ufc.dir/src/validator.cpp.o.d"
+  "CMakeFiles/ufc.dir/src/video_converter.cpp.o"
+  "CMakeFiles/ufc.dir/src/video_converter.cpp.o.d"
+  "ufc"
+  "ufc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ufc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
