81e4824b455653199938dff6d7e6ee252bb70302
Kamal Kumar Kasaudhan <992501030183@mail.jiit.ac.in>
2026-07-03 15:23:34 +0530

    header file image conversion

---
+++ include/image_converter.h
@@ -0,0 +1,18 @@
+#ifndef IMAGE_CONVERTER_H
+#define IMAGE_CONVERTER_H
+
+#include "converter.h"
+
+namespace ufc {
+
+class ImageConverter : public Converter {
+public:
+    std::string validate(const std::string& inputPath, FileType outputType) override;
+    bool convert(const std::string& inputPath, const std::string& outputPath, FileType outputType) override;
+    std::vector<std::pair<FileType, FileType>> supportedFormats() const override;
+    std::string name() const override;
+};
+
+}
+
+#endif
